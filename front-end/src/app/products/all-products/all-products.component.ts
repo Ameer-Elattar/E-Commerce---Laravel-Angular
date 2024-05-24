ee3178c313bb44a29677000ea37f9472b3ee4793
ayaadel1346 <ayaadel1346@gmail.com>
2024-05-24 20:26:33 +0300

    modify design

--- front-end/src/app/products/all-products/all-products.component.ts
+++ front-end/src/app/products/all-products/all-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
 import { FormsModule } from '@angular/forms';
@@ -18,15 +18,53 @@ import { faEye, faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 export class AllProductsComponent implements OnInit, OnDestroy {
   products: Product[] = [];
+  pagedProducts: Product[] = [];
+  currentPage = 1;
+  pageSize = 16; 
   faEye = faEye;
   faCartPlus = faCartPlus;
   private productsSubscription: Subscription | undefined;
-  
+
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.productsSubscription = this.productService.getProducts()
-      .subscribe(products => this.products = products);
+    this.loadProducts();
+  }
+
+
+  loadProducts() {
+    this.productsSubscription = this.productService.getProducts().subscribe(products => {
+      this.products = products;
+      this.setPage(1);
+    });
+  }
+
+  setPage(page: number) {
+    let startIndex: number;
+    let endIndex: number;
+  
+    if (page === 1) {
+      startIndex = 0;
+    } else {
+      startIndex = (page - 1) * this.pageSize;
+    }
+  
+   endIndex = Math.min(startIndex + this.pageSize, this.products.length);
+    this.pagedProducts = this.products.slice(startIndex, endIndex);
+    this.currentPage = page;
+  }
+  
+
+  nextPage() {
+    if (this.currentPage * this.pageSize < this.products.length) {
+      this.setPage(this.currentPage + 1);
+    }
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.setPage(this.currentPage - 1);
+    }
   }
 
   ngOnDestroy() {
@@ -34,8 +72,6 @@ export class AllProductsComponent implements OnInit, OnDestroy {
       this.productsSubscription.unsubscribe();
     }
   }
-
-  
 }
 
 
