6d4bf21ad691ee8a1bf5a87615b8d10315036f66
Your Name <m.abdelazem0202@gmail.com>
2024-05-28 14:12:50 +0300

    update auth guard

--- front-end/src/app/guards/auth.guard.ts
+++ front-end/src/app/guards/auth.guard.ts
@@ -1,14 +1,27 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  let accountService=inject(AuthService);
-  if(accountService.isLoggedIn())
-  return true;
-else {
-let router=inject(Router);
-router.navigateByUrl('/login');
-return false
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.authService.isLoggedIn()) {
+      return true;
+    } else {
+      this.router.navigateByUrl('/login');
+      return false;
+    }
+  }
 }
-};
